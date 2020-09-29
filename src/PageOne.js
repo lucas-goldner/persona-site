30a9ae005aaf346e2be492d03b8c6adf5b2a6532
Lucas Goldner <lucas.goldner@googlemail.com>
2020-09-30 00:02:47 +0200

    PERSONA 2

--- src/PageOne.js
+++ src/PageOne.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import Grid from '@material-ui/core/Grid';
-import Typist from 'react-typist';
+import React from 'react'
+import styled from "styled-components";
 
+const BG = styled.div`
+background-image: url("p3.gif");
+height: 100%; 
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+`
 
-
-const PageOne = () => {
-    return(
+function PageOne() {
+    return (
         <>
-            
+                <BG></BG>
         </>
     )
 }
 
-export default PageOne;
+export default PageOne
