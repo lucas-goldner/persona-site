a8b110177258b3bafae4883b3eeb8cef3ae9c547
Lucas Goldner <lucas.goldner@googlemail.com>
2020-09-30 01:19:44 +0200

    Seems like it works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { createMuiTheme } from '@material-ui/core';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import './App.css';
 import PageOne from './PageOne';
-
 
 function App() {
   return (
--- src/PageOne.js
+++ src/PageOne.js
@@ -1,18 +1,65 @@
 import React from 'react'
 import styled from "styled-components";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Typist from 'react-typist';
 
 const BG = styled.div`
-background-image: url("p3.gif");
-height: 100%; 
-background-position: center;
-background-repeat: no-repeat;
-background-size: cover;
+    background-image: url("p3.gif");
+    height: 100%; 
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
 `
+
+const BGContainer = styled.div`
+    width: 100%;
+    height: 50%;
+    overflow: auto;
+    margin: auto;
+    position: absolute;
+    top: 0; left: 0; bottom: 0; right: 0;
+`
+
+const Title = styled(Typography)`
+    font-family: Roboto !important;
+    font-size: 10rem !important;
+`
+
+const TitleMark = styled.span`
+    color: #282828;
+`
+
+const Cursor = {
+    show: true,
+    blink: true,
+    element: '_',
+    hideWhenDone: true,
+    hideWhenDoneDelay: 1000,
+}
 
 function PageOne() {
     return (
         <>
-                <BG></BG>
+            <BG>
+            <BGContainer>
+            <Grid container justify="center" direction="column">
+                <Grid item>
+                    <Title align="center" color="main" variant="h1">
+                        Hello there
+                    </Title>
+                </Grid>
+                <Grid item>
+                    <Title>
+                        <Typist ms={1000} cursor={Cursor}>
+                            I am <TitleMark>Lucas Goldner</TitleMark>
+                        </Typist>
+                    </Title>
+                </Grid>
+            </Grid>
+            </BGContainer>
+            </BG>
         </>
     )
 }
