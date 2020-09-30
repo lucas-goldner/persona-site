3e3340528cc4038a76152939f84cb9f208993c5f
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-01 01:13:51 +0200

    Homecoming Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { createMuiTheme } from '@material-ui/core';
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-import PageOne from './PageOne';
-import PageTwo from './PageTwo';
+import { createMuiTheme } from "@material-ui/core";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import "./App.css";
+import PageOne from "./PageOne";
+import PageTwo from "./PageTwo";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={PageOne} exact={true}></Route>
+      <Route path="/about" component={PageTwo}></Route>
+    </Switch>
+</BrowserRouter>
+);
 
 function App() {
   return (
-    <div className="App">   
-      <PageOne/>
-      <PageTwo/>
+    <div className="App">
+      <PageOne id="home" />
+      <PageTwo id="about" />
     </div>
   );
 }
--- src/PageTwo.js
+++ src/PageTwo.js
@@ -1,63 +1,79 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import { AppBar, Toolbar, Grid, Typography, List, ListItem, ListItemText} from "@material-ui/core"
-import { makeStyles } from "@material-ui/core/styles"
-import { Home } from "@material-ui/icons"
-import { useHistory } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Link,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Home } from "@material-ui/icons";
 
 const useStyles = makeStyles({
-    navDisplayFlex: {
-      display: `flex`,
-      justifyContent: `start`
-    },
-    linkText: {
-      textDecoration: `none`,
-      textTransform: `uppercase`,
-      color: `white`
-    }
-  });
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `start`,
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`,
+  },
+});
 
 const BG = styled.div`
-    background-color: #E3E2DF;
-`
+  background-color: #e3e2df;
+`;
 
 const TheAppBar = styled(AppBar)`
-    background-color: #5D001E !important;
-`
+  background-color: #5d001e !important;
+`;
+
+const ToolbarLink = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const navLinks = [
-    { title: `About`, path: `/about` },
-    { title: `Projects`, path: `/projects` },
-    { title: `Contact`, path: `/contact` },
-  ]
+  { title: `About`, path: `#about` },
+  { title: `Projects`, path: `#projects` },
+  { title: `Contact`, path: `#contact` },
+];
 
 function PageTwo() {
-    const classes = useStyles(); 
-    let history = useHistory();
-    const handleClick = () => {history.push("/home")}
-
-    return (
-        <>
-        <BG>
-            <TheAppBar position="static" id="about">
-                <Toolbar>
-                <Home edge="start" color="inherit" aria-label="home" onClick={handleClick}>
-                    <Home fontSize="large" />
-                </Home>
-                <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
-                    {navLinks.map(({ title, path }) => (
-                        <a href={path} key={title} className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary={title} />
-                            </ListItem>
-                        </a>
-                    ))}
-                    </List>
-                </Toolbar>
-            </TheAppBar>
-        </BG>    
-        </>
-    )
+  const classes = useStyles();
+  return (
+    <>
+      <BG>
+        <TheAppBar position="static" id="about">
+          <Toolbar>
+            <ToolbarLink href="#" color="inherit">
+              <Home edge="start" color="inherit" aria-label="home">
+                <Home fontSize="large" />
+              </Home>
+            </ToolbarLink>
+            <List
+              component="nav"
+              aria-labelledby="main navigation"
+              className={classes.navDisplayFlex}>
+              {navLinks.map(({ title, path }) => (
+                <a href={path} key={title} className={classes.linkText}>
+                  <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                </a>
+              ))}
+            </List>
+          </Toolbar>
+        </TheAppBar>
+      </BG>
+    </>
+  );
 }
 
-export default PageTwo
+export default PageTwo;
