d53430ae9778e46e401a6d5d22185db78db623f5
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-01 18:41:28 +0200

    Sneaky Swiper Animation Update

--- src/PageTwo.js
+++ src/PageTwo.js
@@ -13,6 +13,7 @@ import {
 import { makeStyles } from "@material-ui/core/styles";
 import { Home } from "@material-ui/icons";
 import SideDrawer from "./SideDrawer";
+import "./PageTwo.css";
 
 const useStyles = makeStyles({
   navDisplayFlex: {
@@ -40,6 +41,40 @@ const ToolbarLink = styled(Link)`
   align-items: center;
 `;
 
+const AboutTitle = styled(Typography)`
+  color: #282828 !important;
+  font-family: Roboto !important;
+  margin-top: 16px !important;
+  position: relative !important;
+
+  animation: slideUp 0.5s 0.5s cubic-bezier(0.5, 0.53, 0.57, 0.53) both,
+    fadeIn 0.6s 0.5s ease-in both;
+  padding: 0 20px;
+  position: absolute;
+  text-align: center;
+  top: 50%;
+
+  @keyframes slideUp {
+    from {
+      transform: translateX(-50%);
+    }
+    to {
+      transform: translateX(0%);
+    }
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+
+
 const navLinks = [
   { title: `About`, path: `#about` },
   { title: `Projects`, path: `#projects` },
@@ -62,19 +97,36 @@ function PageTwo() {
             <List
               component="nav"
               aria-labelledby="main navigation"
-              className={classes.navDisplayFlex}>
-              {touchDevice ? <SideDrawer navLinks={navLinks} /> : (<>
-                {navLinks.map(({ title, path }) => (
-                  <a href={path} key={title} className={classes.linkText}>
-                    <ListItem button>
-                      <ListItemText primary={title} />
-                    </ListItem>
-                  </a>
-                ))}
-                </>)}
+              className={classes.navDisplayFlex}
+            >
+              {touchDevice ? (
+                <SideDrawer navLinks={navLinks} />
+              ) : (
+                <>
+                  {navLinks.map(({ title, path }) => (
+                    <a href={path} key={title} className={classes.linkText}>
+                      <ListItem button>
+                        <ListItemText primary={title} />
+                      </ListItem>
+                    </a>
+                  ))}
+                </>
+              )}
             </List>
           </Toolbar>
         </TheAppBar>
+        <Grid container justify="center" direction="column">
+          <Grid item xs={12}>
+            <div>
+              <AboutTitle variant="h2">About</AboutTitle>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div class="content">
+              <div class="line"></div>
+            </div>
+          </Grid>
+        </Grid>
       </BG>
     </>
   );
