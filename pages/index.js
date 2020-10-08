28c02108d0e3deb81c67acf2ca9154165295e5fe
Lucas Goldner <lucas.goldner@googlemail.com>
2020-10-09 00:38:08 +0200

    FInal Commit until Jakob finds another bug Update

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,17 @@ import PageFour from "../src/PageFour";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Lucas Goldner Persona(l) Site</title>
+        <meta name="author" content="Lucas Goldner" />
+        <meta name="keywords" content="Lucas, Goldner, Personal, Site, Developer, Front-End, Mobile" />
+        <meta
+          name="description"
+          content="Persona(l) site of a Web- and Mobile Developer with all of his projects"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="./favIcon.ico" />
+      </Head>
       <PageOne />
       <PageTwo />
       <PageThree />
